47e2b0d018b5a41fa5efa980d88b45c5e2a2ea20
DK-Mogendar <118730482+DK-Mogendar@users.noreply.github.com>
2023-03-01 15:02:49 +0100

    bilder anpassung

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Opportunity"/>
+ 
 </template>
 
 <script>
@@ -12,15 +12,73 @@ export default {
     HelloWorld
   }
 }
+
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Inter:wght@500;900&display=swap%22");
+body{
+background-color:#E5E5E5;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  text-align:start;
+
   margin-top: 60px;
 }
+.titel {
+    margin: 30px;
+    padding: 20px;
+    font-weight:500;
+    font-Size:28px;
+    line-height:36px;
+    Font:Inter;
+    font-Weight:900;
+    Size:28px;
+    Line-height:36px;
+}
+.app {
+    margin: 30px;
+    padding: 20px;
+    background-color: rgb(34, 13, 109); 
+    color: aliceblue;
+    font-weight:500;
+    font-Size:28px;
+    line-height:36px;
+    Font:Inter;
+    font-Weight:900;
+    Size:28px;
+    Line-height:36px;
+  }
+.farbe1 {
+    color:#EB5E00;
+  }
+.farbe2 {
+    color:#FFBFAB;
+  }
+.farbe3 {
+    color:#FFBFAB;
+  }
+
+.t1 {
+    color: black;
+  }
+.t2 {
+    color:#9AA7B1;
+  }
+
+.footer {
+    background-color: white;
+  }
+.flex-container {
+  display: flex;
+}
+
+.flex-container img {
+  flex-basis: 100%;
+  max-width: 33%;
+  height: auto;
+}
+
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,52 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  
+
+    
+    <header>
+      <div class="titel">
+        <h1 class="t1">{{ msg }}</h1>
+        <h1 class="t2">01.03.2023</h1>
+      </div>
+    </header>
+
+<section>
+  
+  <div class="app">
+        <ul>
+          <li class="farbe1">14.00 UHR</li><br>
+          <li class="farbe2">Basisbeschäftingung Besuch</li><br>
+          <li class="farbe3">Interessierte für den zweiten Kurs</li><br>
+        </ul>
+      </div>
+
+    
+      <div class="app">
+        <ul>
+          <li class="farbe1">14.00 UHR</li><br>
+          <li class="farbe2">Basisbeschäftingung Besuch</li><br>
+          <li class="farbe3">Interessierte für den zweiten Kurs</li><br>
+        </ul>
+      </div>
+
+      <div class="app">
+        <ul>
+          <li class="farbe1">14.00 UHR</li><br>
+          <li class="farbe2">Basisbeschäftingung Besuch</li><br>
+          <li class="farbe3">Interessierte für den zweiten Kurs</li><br>
+        </ul>
+      </div>
+    </section>
+
+  
+
+    <footer class="">
+
+      <div class="flex-container">
+        <img src="../assets/STZH_SEB_Logo.png" alt="Bild 1">
+        <img src="../assets//Opportunity.png" alt="Bild 2">
+        <img src="../assets/SAG_Logo_De.png" alt="Bild 3">
+      </div>
+     </footer>
 </template>
 
 <script>

00db615370d856379729a9171deaec6b90748580
DK-Mogendar <118730482+DK-Mogendar@users.noreply.github.com>
2023-03-02 11:08:40 +0100

    anpassung mit chris

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,73 @@
 <template>
-  <HelloWorld msg="Welcome to Opportunity"/>
- 
+  <div id="app">
+    <h1 class="site-title">{{ title }}</h1>
+    <span class="site-description">{{ currentDate }}</span>
+
+    <!-- entry list -->
+    <ul v-if="entries" class="entry-list" >
+      <li v-for="entry in entries" class="entry-item"  :key="entry.id">
+        <span class="entry-daytime">02/03/2023 {{entry[1] }} {{entry[0] }} Uhr</span><br />
+        <h3 class="entry-title">{{ entry[2]}}</h3>
+        <span class="entry-description">{{ entry[3]}}</span><br />
+      </li>
+    </ul>
+
+    <!--Text wird nur angezeigt wenn keine einträge vorhanden-->
+    <h1 v-else>Keine Events zur Zeit vorhanden!</h1>
+
+    <!-- footer -->
+    <footer class="footer">
+      <img
+        src="./assets/STZH_SEB_Logo.png"
+        alt="Stadt Zürich Soziale Betriebe Logo"
+      >
+      <img
+        src="./assets/Opportunity.png"
+        alt="Opportunity Zürich Logo"
+      >
+      <img
+        src="./assets/SAG_Logo_De.png"
+        alt="Stiftung Arbeitsgestaltung Logo"
+      >
+    </footer>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      title: "Welcome to Opportunity",
+      currentDate: "",
+      entries: []
+    };
+  },
 
+  methods: {
+    getData() {
+      this.entries =  [
+        ["8:25", "08/09/2022", "Auaaa wirklich", "Alles zum thema Auaaaaaa"],
+        ["17:25", "07/03.2023", "Coole sache", "alles zu Cool"],
+        ["17:25", "07/03.2023", "Coole sache", "alles zu Cool"]
+      ];
+    },
+    updateCurrentDate() {
+      let today = new Date();
+      this.counter++;
+      this.currentDate = `${today.getDate()}.${today.getMonth() + 1}.${today.getFullYear()}`;
+    },
+    refreshData() {
+      this.updateCurrentDate();
+      this.getData();
+    },
+  },
+  mounted() {
+    this.refreshData(); // get first initial data and then wait for the next
+    setInterval(this.refreshData, 60000); // wait one minute for next update (1000 * 60)
+  },
+};
 </script>
 
 <style>
@@ -32,20 +87,22 @@ background-color:#e8eff4;
 .header {
   margin: 30px;
 }
-.title {
+.site-title {
+  color: black;
   font-size: 62px;
   font-weight: 900;
   margin: 80px 0 20px 0;
 }
 
-.untertitel{
+.site-description{
+  color:#9AA7B1;
   font-size: 62px;
   color: #9aa7b1;
   font-weight: 500;
   margin: 0;
   
 }
-.app {
+.entrie {
     margin: 30px;
     padding: 20px;
     background-color: rgb(34, 13, 109); 
@@ -68,12 +125,6 @@ background-color:#e8eff4;
     color:#FFBFAB;
   }
 
-.t1 {
-    color: black;
-  }
-.t2 {
-    color:#9AA7B1;
-  }
 
 .footer {
   display: flex;

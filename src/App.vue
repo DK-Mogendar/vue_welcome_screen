9ad197fe655683d5338941e1cb1722c14521ae29
DK-Mogendar <118730482+DK-Mogendar@users.noreply.github.com>
2023-03-07 10:00:59 +0100

    ????

--- src/App.vue
+++ src/App.vue
@@ -10,19 +10,6 @@
     <ul v-if="entries && entries.length" class="entry-list" >
       
       <li class="entry-item"  v-for="entry in entries" :key="entry.id">
-
-<!------------------------------------------------------------------------------------------------------------>
-      <!--Dieser Code durchläuft die Liste "entries" und rendert für jedes Element einen Listenpunkt (li)
-      mit der CSS-Klasse "entry-item". Der Schlüssel des Listenpunkts wird auf die ID des Eintrags gesetzt, 
-      um sicherzustellen, dass Vue.js jedes Element korrekt verfolgen kann. Der Inhalt des Listenpunkts wird nicht angezeigt, 
-      da er nicht Teil des Pseudocodes ist.-->
-
-      <!-- <li v-for="entry in entries" class="entry-item"  :key="entry.id">
-        <span class="entry-daytime">{{entry[0] }} Uhr, {{entry[1].replaceAll("/", ".")}}</span><br />
-        <h3 class="entry-title">{{ entry[2]}}</h3>
-        <span class="entry-description">{{ entry[3]}}</span><br />-->
-<!------------------------------------------------------------------------------------------------------------->
-
         <EventEntry :entry="entry"/>
       </li>
     </ul>
@@ -51,92 +38,86 @@
 
 <script>
 import axios from "axios"; //axios is a libary for making HTTP rquest to the backend
+import EventEntry from "./components/EventEntry.vue";
 
 //Definiert dei einzelnen Variablen der App
 export default {
-  name: "App",
-  data() {
-    return {
-      title: "Welcome to Opportunity",
-                    /*Meine Liste */                                         /*Chris Liste */
-      sheet_id:"1ycTdsvR4P08qDw9p0rmN73rqtqdXGdwxYDLKEGgEnpc"/*"1CR1UKN0LAPNs6lWbfA2gBI2FazmWdVSFIzIwi5TG5Z4"*/,
-      api_token:"AIzaSyAcQYdWwMV5KCzZIKI-gozSS9osnZ5CQsE"/*"AIzaSyA-qeDXOhEeQDA0vQf7LgkF7DQtGnAtmAU"*/,
-      currentDate: "",
-      entries: []
-    };
-  },
-
-  computed: {
-    /*Dieser Code definiert eine Eigenschaft gsheet_url, die eine URL zurückgibt, die auf eine bestimmte Tabelle 
-    in einer Google Sheets-Datei verweist. Die URL wird mit den Informationen sheet_id und api_token generiert, 
-    die von der Klasse verwendet werden, in der sich dieser Code befindet.
-    (ChrisBeschreibung).
-    Berechnete Eigenschaften sind wie Dateneigenschaften, 
-    aber mit einer kombinierten Methode, die automatisch ausgeführt wird, anstatt explizit eine Funktion aufzurufen. */
-    gsheet_url() {
-      return `https://sheets.googleapis.com/v4/spreadsheets/${this.sheet_id}/values:batchGet?ranges=A2%3AE100&valueRenderOption=FORMATTED_VALUE&key=${this.api_token}`;
+    name: "App",
+    components: { EventEntry },
+    data() {
+        return {
+            title: "Welcome to Opportunity",
+            /*Meine Liste */ /*Chris Liste */
+            sheet_id: "1ycTdsvR4P08qDw9p0rmN73rqtqdXGdwxYDLKEGgEnpc" /*"1CR1UKN0LAPNs6lWbfA2gBI2FazmWdVSFIzIwi5TG5Z4"*/,
+            api_token: "AIzaSyAcQYdWwMV5KCzZIKI-gozSS9osnZ5CQsE" /*"AIzaSyA-qeDXOhEeQDA0vQf7LgkF7DQtGnAtmAU"*/,
+            currentDate: "",
+            entries: []
+        };
     },
-  },
-
-  methods: {
-    getData() {
-      /*BEGINN
-      FUNKTION getData()
-      Rufen Sie axios.get() mit this.gsheet_url als Argument auf.
-      Warted auf die Antwort.
-      Wenn die Antwort erfolgreich ist, dann
-      setze this.entries auf die Werte des ersten Wertebereichs in response.data
-      ENDWENN
-      ENDFUNKTION
-      END*/
-      
-        axios.get(this.gsheet_url).then((response) => {
-          this.entries = response.data.valueRanges[0]. values;
-        });
-     
-
-      /*this.entries =  [
-        ["8:25", "08/09/2022", "Auaaa wirklich", "Alles zum thema Auaaaaaa"],
-        ["17:25", "07/03.2023", "Coole sache", "alles zu Cool"],
-        ["19:25", "08/03.2023", "Coole sache zwei", "alles zu Cool zwei"]
-      ]; */
-
+    computed: {
+        /*Dieser Code definiert eine Eigenschaft gsheet_url, die eine URL zurückgibt, die auf eine bestimmte Tabelle
+        in einer Google Sheets-Datei verweist. Die URL wird mit den Informationen sheet_id und api_token generiert,
+        die von der Klasse verwendet werden, in der sich dieser Code befindet.
+        (ChrisBeschreibung).
+        Berechnete Eigenschaften sind wie Dateneigenschaften,
+        aber mit einer kombinierten Methode, die automatisch ausgeführt wird, anstatt explizit eine Funktion aufzurufen. */
+        gsheet_url() {
+            return `https://sheets.googleapis.com/v4/spreadsheets/${this.sheet_id}/values:batchGet?ranges=A2:E100&valueRenderOption=FORMATTED_VALUE&key=${this.api_token}`;
+        },
+    },
+    methods: {
+        getData() {
+            /*BEGINN
+            FUNKTION getData()
+            Rufen Sie axios.get() mit this.gsheet_url als Argument auf.
+            Warted auf die Antwort.
+            Wenn die Antwort erfolgreich ist, dann
+            setze this.entries auf die Werte des ersten Wertebereichs in response.data
+            ENDWENN
+            ENDFUNKTION
+            END*/
+            axios.get(this.gsheet_url).then((response) => {
+                this.entries = response.data.valueRanges[0].values;
+            });
+            /*this.entries =  [
+              ["8:25", "08/09/2022", "Auaaa wirklich", "Alles zum thema Auaaaaaa"],
+              ["17:25", "07/03.2023", "Coole sache", "alles zu Cool"],
+              ["19:25", "08/03.2023", "Coole sache zwei", "alles zu Cool zwei"]
+            ]; */
+            /*Beginn mit der
+            Funktion updateCurrentDate()
+            setze today auf ein neues Datum-Objekt
+            Inkementiere this.counter um 1
+            setze this.currentDate auf einen String, der das heutige Datum im Format "Tag.Monat.Jahr" darstellt
+            ENDFUNKTION
+            END*/
+        },
+        updateCurrentDate() {
+            let today = new Date();
+            this.counter++;
+            this.currentDate = `${today.getDate()}.${today.getMonth() + 1}.${today.getFullYear()}`;
+        },
+        /*Beginn mit der
+        Funktion refreshData()
+        ruf das updateCurrentDate() auf
+        ruf das getData() auf
+        ENDFUNKTION
+        END */
+        refreshData() {
+            this.updateCurrentDate();
+            this.getData();
+        },
+    },
     /*Beginn mit der
-    Funktion updateCurrentDate()
-    setze today auf ein neues Datum-Objekt
-    Inkementiere this.counter um 1
-    setze this.currentDate auf einen String, der das heutige Datum im Format "Tag.Monat.Jahr" darstellt
+    Funktion mounted()
+    ruf das refreshData() auf, um die ersten Daten abzurufen und auf das nächste Update zu warten
+    seze den Intervall-Timer auf, der alle 18000000 Millisekunden die refreshData() Methode ausführt
     ENDFUNKTION
-    END*/ 
-    },
-    updateCurrentDate() {
-      let today = new Date();
-      this.counter++;
-      this.currentDate = `${today.getDate()}.${today.getMonth() + 1}.${today.getFullYear()}`;
-    },
-
-  /*Beginn mit der
-  Funktion refreshData()
-  ruf das updateCurrentDate() auf
-  ruf das getData() auf
-  ENDFUNKTION
-  END */  
-    refreshData() {
-      this.updateCurrentDate();
-      this.getData();
-    },
-  },
-
-  /*Beginn mit der
-  Funktion mounted()
-  ruf das refreshData() auf, um die ersten Daten abzurufen und auf das nächste Update zu warten
-  seze den Intervall-Timer auf, der alle 18000000 Millisekunden die refreshData() Methode ausführt
-  ENDFUNKTION
-  END */
-  mounted() {
-    this.refreshData(); // get first initial data and then wait for the next
-    setInterval(this.refreshData, 18000000); // wait one minute for next update (1000 * 60)
-  },
+    END */
+    mounted() {
+        this.refreshData(); // get first initial data and then wait for the next
+        setInterval(this.refreshData, 18000000); // wait one minute for next update (1000 * 60)
+    }
 };
 </script>
 

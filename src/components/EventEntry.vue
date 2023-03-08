b394c0b6b64c97bfa58fb1bf5485fbe3cb5d7615
DK-Mogendar <118730482+DK-Mogendar@users.noreply.github.com>
2023-03-08 13:27:04 +0100

    schluss

--- src/components/EventEntry.vue
+++ src/components/EventEntry.vue
@@ -1,15 +1,19 @@
 <template>
+    <!-- Anzeige der Tageszeit des Eintrags im 24-Stunden-Format plus das Datum-->
     <span class="entry-daytime">{{entry[0]}} Uhr, {{entry[1].replaceAll("/", ".")}}</span><br />
+    <!--Anzeige des Titels-->
     <h3 class="entry-title">{{entry[2]}}</h3>
+    <!--Anzeige der anstehenden Aufgabe-->
     <span class="entry-description">{{entry[3]}}</span><br />
 </template>
 
-<script>
+<script>//Exportiert das Objekt vom vordefinierten Modul (Export-Objekt)
 export default {
+  //Props (Vue.js-Komponente) deklariert Parameter zur definition,von übergeordneten Komponenten die übergeben werden können.
   props: ['entry'],
-  data() {
+  data() {//Giebt die zusammengestellten Daten aus.
     return {
-      // counter: 0
+      // counter: 0 (Altes Beispiel)
     }
   }
 }
